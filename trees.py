b4b12451a05eee8406307f9273a6b858cfba2703
Charles Arthur <charles.v.arthur@gmail.com>
2022-12-21 11:02:50 +0000

    updated the dataframe

--- trees.py
+++ trees.py
@@ -5,11 +5,10 @@
 import streamlit as st
 import pandas as pd
 
-
 st.title = ('SF Trees')
 st.write = ('This app analyses the trees in San Francisco using '
             ' a dataset kindly provided by SG DPW')
 
-trees_df = ('https://raw.githubusercontent.com/tylerjrichards/Streamlit-for-Data-Science/main/trees_app/trees.csv')
+trees_df = pd.read_csv('https://raw.githubusercontent.com/tylerjrichards/Streamlit-for-Data-Science/main/trees_app/trees.csv')
 
 st.write(trees_df.head())
