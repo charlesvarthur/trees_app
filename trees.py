5e19602f9625e156699596767f4cfcd07fe71004
Charles Arthur <charles.v.arthur@gmail.com>
2022-12-21 11:00:37 +0000

    test dataset and app config

--- trees.py
+++ trees.py
@@ -2,3 +2,14 @@
 # Trees App #
 #############
 
+import streamlit as st
+import pandas as pd
+
+
+st.title = ('SF Trees')
+st.write = ('This app analyses the trees in San Francisco using '
+            ' a dataset kindly provided by SG DPW')
+
+trees_df = ('https://raw.githubusercontent.com/tylerjrichards/Streamlit-for-Data-Science/main/trees_app/trees.csv')
+
+st.write(trees_df.head())
