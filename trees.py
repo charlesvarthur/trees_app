660fdfb02ab6e77ddb3e679746791b2628558860
Charles Arthur <charles.v.arthur@gmail.com>
2022-12-21 11:11:07 +0000

    updated raw link

--- trees.py
+++ trees.py
@@ -6,8 +6,7 @@ import streamlit as st
 import pandas as pd
 
 st.title('SF Trees')
-st.write('This app analyses the trees in San Francisco using '
-            ' a dataset kindly provided by SG DPW')
+st.write('This app analyses the trees in San Francisco using a dataset kindly provided by SG DPW')
 
 trees_df = pd.read_csv('https://raw.githubusercontent.com/tylerjrichards/Streamlit-for-Data-Science/main/trees_app/trees.csv')
 
