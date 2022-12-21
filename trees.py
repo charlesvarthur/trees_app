926031bd575444c15dc8d82fe98a6a21e82e6cc5
Charles Arthur <charles.v.arthur@gmail.com>
2022-12-21 11:31:35 +0000

    line bar and area chart of the tree dbh counts

--- trees.py
+++ trees.py
@@ -5,10 +5,19 @@
 import streamlit as st
 import pandas as pd
 
+#Title and opening paragraph
 st.title("SF Trees")
 st.write('This app analyses the trees in San Francisco using'
            ' a dataset kindly provided by SG DPW')
 
+#Import dataset
 trees_df = pd.read_csv('https://raw.githubusercontent.com/tylerjrichards/Streamlit-for-Data-Science/main/trees_app/trees.csv')
 
-st.write(trees_df.head())
+#st.write(trees_df.head())
+
+df_dbh_grouped = pd.DataFrame(trees_df.groupby(['dbh']).count()['tree_id'])
+df_dbh_grouped.columns = ['tree_count']
+
+st.line_chart(df_dbh_grouped)
+st.bar_chart(df_dbh_grouped)
+st.area_chart(df_dbh_grouped)
